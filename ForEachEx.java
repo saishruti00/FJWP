fffcb177da1e5599f13e88cc88860f3a0a2522fe
saishruti00 <140233114+saishruti00@users.noreply.github.com>
2024-06-04 16:40:12 +0530

    Add files via upload

---
+++ ForEachEx.java
@@ -0,0 +1,20 @@
+package day6;
+
+public class ForEachEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]  a= {1,2,3};
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println("\n-----");
+		for(int i:a)
+		{
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
